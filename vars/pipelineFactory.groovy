7401c786fd1d0b0d651db42dc7910363d658d3f8
xinnj <xinnj@hotmail.com>
2022-11-20 23:59:38 +0800

    Get version

--- vars/pipelineFactory.groovy
+++ vars/pipelineFactory.groovy
@@ -4,6 +4,21 @@ Map call(Map config) {
 
     def clsLoader = this.getClass().classLoader
     String scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
+
+//    String command = "git clone https://gitee.com/xinnj/iDO-Pipelines.git " + scriptDir + "/test"
+//    echo command
+//    echo command.execute().text
+//    command = "ls -l " + scriptDir
+//    echo command
+//    echo command.execute().text
+
+    File file = new File(scriptDir + "/t.sh")
+    file.write "export w='hello'\n"
+    file << "echo \$w\n"
+    command = "bash " + scriptDir + "/t.sh"
+    echo command
+    echo command.execute().text
+
     clsLoader.addClasspath(scriptDir + "/../src/com/ido/pipeline/")
     pipeline = clsLoader.loadClass(config.pipelineType, true, false)?.newInstance(this)
 
