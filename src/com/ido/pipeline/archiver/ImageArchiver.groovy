3634bdb358e878d6a247e492e9b5add35a1ebf9f
xinnj <xinnj@hotmail.com>
2024-04-10 22:03:00 +0800

    Check helm upload result

--- src/com/ido/pipeline/archiver/ImageArchiver.groovy
+++ src/com/ido/pipeline/archiver/ImageArchiver.groovy
@@ -113,8 +113,13 @@ public class ImageArchiver {
 
         steps.withCredentials([steps.usernameColonPassword(credentialsId: config.helm.uploadCredentialId, variable: 'USERPASS')]) {
             steps.container('helm') {
-                steps.sh """${config.debugSh}
-                    curl -u "\${USERPASS}" "${config.helm.repo}" -v --upload-file "${chartName}-${config.helm.chartVersion}.tgz"
+                steps.sh """#!/bin/bash
+                    ${config.debugSh}
+                    result=\$(curl -u "\${USERPASS}" "${config.helm.repo}" -v --upload-file "${chartName}-${config.helm.chartVersion}.tgz" --write-out '%{http_code}')
+                    if [[ "\$result" =~ ^[^23] ]]; then
+                        echo "Upload Helm chart failed!"
+                        exit 1
+                    fi
                 """
             }
         }
