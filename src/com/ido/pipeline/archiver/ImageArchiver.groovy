212140c7de326bd0f8d21c58af4e4ac238ea993c
xinnj <xinnj@hotmail.com>
2024-05-20 18:26:44 +0800

    Fix helm upload

--- src/com/ido/pipeline/archiver/ImageArchiver.groovy
+++ src/com/ido/pipeline/archiver/ImageArchiver.groovy
@@ -113,10 +113,10 @@ public class ImageArchiver {
 
         steps.withCredentials([steps.usernameColonPassword(credentialsId: config.helm.uploadCredentialId, variable: 'USERPASS')]) {
             steps.container('helm') {
-                steps.sh """#!/bin/bash
-                    ${config.debugSh}
+                steps.sh """${config.debugSh}
                     result=\$(curl -u "\${USERPASS}" "${config.helm.repo}" -v --upload-file "${chartName}-${config.helm.chartVersion}.tgz" --write-out '%{http_code}')
-                    if [[ "\$result" =~ ^[^23] ]]; then
+                    prefix=\$(echo \$result | cut -c -1)
+                    if [ "\$prefix" != "2" ] && [ "\$prefix" != "3" ]; then
                         echo "Upload Helm chart failed!"
                         exit 1
                     fi
